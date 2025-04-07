2f9573692e196d10c3a475f1b60f9352f2a21c19
tavkeer <shahtavkeer@gmail.com>
2025-04-07 07:22:48 +0530

    FEAT:artisans table implemented

---
+++ src/app/artisans/type/artisan.type.ts
@@ -0,0 +1,10 @@
+type Artisan = {
+    id: string;
+    name: string;
+    image: string;
+    address?: string;
+    phone?: string;
+    story?: string;
+    created_at?: string;
+    updated_at?: string;
+  };
